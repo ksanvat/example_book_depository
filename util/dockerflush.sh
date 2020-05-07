4df23f462d0d99fe2484bd206a61713f22f5ee37
ksanvat <ksanvat@gmail.com>
2020-05-07 16:16:38 +0300

    fixed dockerflush.sh

--- util/dockerflush.sh
+++ util/dockerflush.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-docker image prune --all --force > /dev/null
-docker image ls --all | [[ $(wc -l) -eq "1" ]]
-if [[ "$?" != "0" ]]; then
-    echo "Images are not empty"
-    exit 1
-fi
-
 docker container prune --force > /dev/null
 docker container ls --all | [[ $(wc -l) -eq "1" ]]
 if [[ "$?" != "0" ]]; then
     echo "Containers are not empty"
+    exit 1
+fi
+
+docker image prune --all --force > /dev/null
+docker image ls --all | [[ $(wc -l) -eq "1" ]]
+if [[ "$?" != "0" ]]; then
+    echo "Images are not empty"
     exit 1
 fi
 
